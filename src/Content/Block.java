4ac71023cfcaf20df5b52b9b0aab5ddde702fae4
CuteTiaLu <3562644513@qq.com>
2026-09-14 01:18:14 +0800

    init

--- src/Content/Block.java
+++ src/Content/Block.java
@@ -40,21 +40,14 @@ public class Block {
         Reaction_Power_Plant = new ConsumeGenerator("反应发电厂") {{
             powerProduction = 12580f / 60f;
             size = 5;
-            researchCost = with(Category.power, ItemStack.with(Items.thorium, 320, Items.silicon, 30, Items.tungsten, 450, Items.oxide, 80));
+            requirements(Category.power, ItemStack.with(Items.thorium, 320, Items.silicon, 30, Items.tungsten, 450, Items.oxide, 80));
             health = 35000;
             description = "";
             generateEffect = Fx.steam;
             consumeEffect = Fx.explosion;
             itemDuration = 8f * 60f;
-            consumeLiquids(LiquidStack.with(Liquids.ozone, 8f / 60f, Liquids.arkycite, 120f / 60f, Liquids.hydrogen, 16f / 60f));
-            drawer = new DrawMulti(new DrawRegion("-底"), new DrawPistons(){{
-                sinMag = 3f;
-                sinScl = 5f;
-            }}, new DrawRegion("-液体"), new DrawLiquidTile(Liquids.arkycite, 37f / 4f), new DrawDefault(), new DrawGlowRegion(){{
-                alpha = 1f;
-                glowScale = 5f;
-                color = Color.valueOf("c967b099");
-            }});
+            consumeLiquids(LiquidStack.with(Liquids.ozone, 8f / 60f, Liquids.arkycite, 180f / 60f, Liquids.hydrogen, 24f / 60f));
+            drawer = new DrawMulti(new DrawRegion("-底"), new DrawRegion("-液体"), new DrawDefault());
         }};
     }
 }
